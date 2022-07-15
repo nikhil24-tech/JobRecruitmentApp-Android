238a84db8e2d2cef02d57bb4baae489e79262845
shamlisampla <shamlisampla@gmail.com>
2022-07-15 08:14:18 -0400

    Job List UI

---
+++ app/src/main/java/com/example/jobrecruitmentapp_android/JobListFragment.java
@@ -0,0 +1,72 @@
+package com.example.jobrecruitmentapp_android;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jobrecruitmentapp_android.databinding.FragmentJobListBinding;
+import com.example.jobrecruitmentapp_android.placeholder.PlaceholderContent;
+
+
+public class JobListFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    private FragmentJobListBinding binding;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public JobListFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static JobListFragment newInstance(int columnCount) {
+        JobListFragment fragment = new JobListFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentJobListBinding.inflate(inflater, container, false);
+        binding.list.setLayoutManager(new LinearLayoutManager(binding.getRoot().getContext()));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NavController navController = Navigation.findNavController(requireView());
+        binding.list.setAdapter(new MyJobRecyclerViewAdapter(navController, PlaceholderContent.ITEMS));
+    }
+}
