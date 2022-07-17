8b3503655a1547b9bec989ddc704a505f7dfd19e
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-07-17 18:08:22 -0400

    Applied Job UI

--- app/src/main/java/com/example/jobrecruitmentapp_android/AppliedJobFragment.java
+++ app/src/main/java/com/example/jobrecruitmentapp_android/AppliedJobFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.jobrecruitmentapp_android.databinding.FragmentSavedJobBinding;
 import com.example.jobrecruitmentapp_android.placeholder.PlaceholderContent;
 
