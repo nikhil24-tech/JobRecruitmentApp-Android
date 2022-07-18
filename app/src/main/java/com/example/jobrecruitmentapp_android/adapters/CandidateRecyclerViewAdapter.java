f2c567fd3bd67d1e45194d7d68a27beaf4a28c63
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-07-18 10:32:45 -0400

    navigation and  candidate details.

---
+++ app/src/main/java/com/example/jobrecruitmentapp_android/adapters/CandidateRecyclerViewAdapter.java
@@ -0,0 +1,74 @@
+package com.example.jobrecruitmentapp_android.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.jobrecruitmentapp_android.R;
+import com.example.jobrecruitmentapp_android.databinding.ItemCandidateBinding;
+import com.example.jobrecruitmentapp_android.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+
+public class CandidateRecyclerViewAdapter extends RecyclerView.Adapter<CandidateRecyclerViewAdapter.ViewHolder> {
+
+    private final List<User> mValues;
+    private final Consumer<User> onUserSelected;
+    private final NavController navController;
+
+
+    public CandidateRecyclerViewAdapter(NavController navController, Consumer<User> onUserSelected) {
+        mValues = new ArrayList<>();
+        this.navController = navController;
+        this.onUserSelected = onUserSelected;
+    }
+
+    public void submitList(List<User> users) {
+        mValues.clear();
+        mValues.addAll(users);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(ItemCandidateBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.user = mValues.get(position);
+        holder.binding.name.setText(holder.user.name);
+        holder.binding.phone.setText(holder.user.phone);
+        holder.binding.location.setText(holder.user.address);
+        if (holder.user.userType != null) {
+            holder.binding.userType.setText(holder.user.userType.toUpperCase(Locale.ROOT));
+        }
+        holder.binding.viewCandidate.setOnClickListener(v -> {
+            onUserSelected.accept(holder.user);
+            navController.navigate(R.id.admin_action_navigation_saved_to_candidateDetailFragment);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ItemCandidateBinding binding;
+        public User user;
+
+        public ViewHolder(ItemCandidateBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
