94e2570f5d46eaf8cec9c90fe3fccb8fdfa7523b
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-07-29 01:46:08 -0400

    Candidate Details

--- app/src/main/java/com/example/jobrecruitmentapp_android/fragments/CandidateDetailsFragment.java
+++ app/src/main/java/com/example/jobrecruitmentapp_android/fragments/CandidateDetailsFragment.java
@@ -1,9 +1,13 @@
 package com.example.jobrecruitmentapp_android.fragments;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +16,10 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.jobrecruitmentapp_android.databinding.FragmentCandidateDetailsBinding;
 import com.example.jobrecruitmentapp_android.viewmodels.UserViewModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CandidateDetailsFragment extends Fragment {
 
@@ -36,17 +44,49 @@ public class CandidateDetailsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
-        viewModel.getSelectedUser().observe(getViewLifecycleOwner(), user -> {
-            binding.userName.setText(user.name);
-            binding.location.setText(user.address);
-            binding.address.setText(user.address);
-            binding.experience.setText(user.jobExperience);
-            binding.skills.setText(user.skills);
-            binding.aboutMe.setText(user.aboutMe);
-            binding.phone.setText(user.phone);
-            binding.email.setText(user.email);
-            binding.blockUser.setVisibility(View.GONE);
-            binding.unblockUser.setVisibility(View.GONE);
+        viewModel.getCurrentUser().observe(getViewLifecycleOwner(), currentUser -> {
+            if (currentUser == null) {
+                return;
+            }
+            viewModel.getSelectedUser().observe(getViewLifecycleOwner(), user -> {
+                binding.userName.setText(user.name);
+                binding.location.setText(user.address);
+                binding.address.setText(user.address);
+                binding.experience.setText(user.jobExperience);
+                binding.skills.setText(user.skills);
+                binding.aboutMe.setText(user.aboutMe);
+                binding.phone.setText(user.phone);
+                binding.email.setText(user.email);
+                if (currentUser.userType != null && currentUser.userType.equalsIgnoreCase("admin")) {
+                    int color = binding.getRoot().getContext().getColor(android.R.color.holo_red_dark);
+                    binding.blockUser.setBackgroundTintList(ColorStateList.valueOf(color));
+                    binding.blockUser.setOnClickListener(v -> {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("isBlocked", true);
+                        FirebaseFirestore
+                                .getInstance()
+                                .collection("jk_users")
+                                .document(user.uid)
+                                .update(map)
+                                .addOnSuccessListener(task -> Toast.makeText(requireContext(), "User blocked!", LENGTH_SHORT).show())
+                                .addOnFailureListener(task -> Toast.makeText(requireContext(), "Unable to block user!", LENGTH_SHORT).show());
+                    });
+                    binding.unblockUser.setOnClickListener(v -> {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("isBlocked", false);
+                        FirebaseFirestore
+                                .getInstance()
+                                .collection("jk_users")
+                                .document(user.uid)
+                                .update(map)
+                                .addOnSuccessListener(task -> Toast.makeText(requireContext(), "User unblocked!", LENGTH_SHORT).show())
+                                .addOnFailureListener(task -> Toast.makeText(requireContext(), "Unable to unblock user!", LENGTH_SHORT).show());
+                    });
+                } else {
+                    binding.blockUser.setText("Reject");
+                    binding.unblockUser.setText("Approve");
+                }
+            });
         });
     }
 }
