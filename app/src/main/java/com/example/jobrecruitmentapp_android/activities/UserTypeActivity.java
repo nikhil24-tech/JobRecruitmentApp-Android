28d31555b49b797c9fe589547b1cb59e918c67bc
nikhil24-tech <84915715+nikhil24-tech@users.noreply.github.com>
2022-08-14 15:47:40 -0400

    SignIn & Signup Updated.(Fixed))

--- app/src/main/java/com/example/jobrecruitmentapp_android/activities/UserTypeActivity.java
+++ app/src/main/java/com/example/jobrecruitmentapp_android/activities/UserTypeActivity.java
@@ -1,9 +1,9 @@
 package com.example.jobrecruitmentapp_android.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.jobrecruitmentapp_android.databinding.ActivityUserTypeBinding;
 
@@ -18,20 +18,20 @@ public class UserTypeActivity extends AppCompatActivity {
         binding = ActivityUserTypeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.Admin.setOnClickListener(v -> openLogInMenu("Admin"));
-        binding.Employer.setOnClickListener(v -> openSignInMenu("Employer"));
-        binding.JobSeeker.setOnClickListener(v -> openSignInMenu("JobSeeker"));
+        binding.adminButton.setOnClickListener(v -> openLogin("admin"));
+        binding.employerButton.setOnClickListener(v -> openSignInMenu("employer"));
+        binding.jobseekerButton.setOnClickListener(v -> openSignInMenu("jobseeker"));
     }
 
-    void openSignInMenu(String userType){
-        Intent intent = new Intent(this, SignupOptionsActivity.class);
+    void openLogin(String userType) {
+        Intent intent = new Intent(this, EmailLoginActivity.class);
         intent.putExtra("userType", userType);
         startActivity(intent);
         finish();
     }
 
-    void openLogInMenu(String userType) {
-        Intent intent = new Intent(this, EmailLoginActivity.class);
+    void openSignInMenu(String userType) {
+        Intent intent = new Intent(this, SignupOptionsActivity.class);
         intent.putExtra("userType", userType);
         startActivity(intent);
         finish();
