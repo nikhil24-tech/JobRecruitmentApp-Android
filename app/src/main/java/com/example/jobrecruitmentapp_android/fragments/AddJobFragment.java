ced1980c0c284ba7f9b922260c874b1f51920314
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-08-16 01:43:52 -0400

    Edit Job

--- app/src/main/java/com/example/jobrecruitmentapp_android/fragments/AddJobFragment.java
+++ app/src/main/java/com/example/jobrecruitmentapp_android/fragments/AddJobFragment.java
@@ -9,15 +9,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.jobrecruitmentapp_android.R;
 import com.example.jobrecruitmentapp_android.databinding.FragmentAddJobBinding;
 import com.example.jobrecruitmentapp_android.viewmodels.UserViewModel;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -27,7 +25,8 @@ public class AddJobFragment extends Fragment {
 
     private FragmentAddJobBinding binding;
 
-    public AddJobFragment() {}
+    public AddJobFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,39 +43,37 @@ public class AddJobFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         NavController navController = Navigation.findNavController(requireView());
+        UserViewModel viewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
 
-        binding.addJobButton.setOnClickListener(v -> {
-            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        viewModel.getCurrentUser().observe(getViewLifecycleOwner(), user -> {
+            binding.addJobButton.setOnClickListener(v -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("jobName", binding.nameTextField.getEditText().getText().toString());
+                map.put("jobAddress", binding.addressTextField.getEditText().getText().toString());
+                map.put("jobLocation", user.orgLocation);
+                map.put("empEmail", user.email);
+                map.put("empPhone", binding.contactTextField.getEditText().getText().toString());
+                map.put("jobDescription", binding.descriptionTextField.getEditText().getText().toString());
+                map.put("jobRequirements", binding.requirementsTextField.getEditText().getText().toString());
+                map.put("orgImageUrl", user.orgImageUrl);
+                map.put("orgName", user.orgName);
+                map.put("orgType", binding.organisationTypeTextField.getEditText().getText().toString());
+                map.put("salaryPerHr", binding.salaryTextField.getEditText().getText().toString());
 
-            Map<String, Object> map = new HashMap<>();
-            map.put("jobName", binding.nameTextField.getEditText().getText().toString());
-            map.put("jobAddress", binding.addressTextField.getEditText().getText().toString());
-            map.put("jobLocation", binding.addressTextField.getEditText().getText().toString());
-            map.put("jobEmail", binding.emailTextField.getEditText().getText().toString());
-            map.put("orgType", binding.organisationTypeTextField.getEditText().getText().toString());
-            map.put("jobRequirements", binding.requirementsTextField.getEditText().getText().toString());
-            map.put("jobDescription", binding.descriptionTextField.getEditText().getText().toString());
-            map.put("salaryPerHr", binding.salaryTextField.getEditText().getText().toString());
-            map.put("createdBy", uid);
-
-            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-            DocumentReference newJobRef = firestore.collection("jobs").document();
-            DocumentReference currentUserRef = firestore.collection("jk_users").document(uid);
-
-            firestore.batch()
-                    .set(newJobRef, map)
-                    .update(currentUserRef, "postedJobs", FieldValue.arrayUnion(newJobRef.getId()))
-                    .commit()
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(requireContext(), "Job added!", Toast.LENGTH_SHORT).show();
-                            navController.navigate(R.id.action_addJobFragment_to_employer_navigation_job_modify);
-                        } else {
-                            Toast.makeText(requireContext(), "Could not add job!", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                FirebaseFirestore
+                        .getInstance()
+                        .collection("jobs")
+                        .add(map)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(requireContext(), "Job added!", Toast.LENGTH_SHORT).show();
+                                navController.navigate(R.id.action_addJobFragment_to_employer_navigation_job_modify);
+                            } else {
+                                Toast.makeText(requireContext(), "Could not add job!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            });
         });
     }
 }
